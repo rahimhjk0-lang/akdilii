45104f1b709e1b780d637f797e5b139c3e30299f
rahimhjk0-lang <rahimhjk0@gmail.com>
2026-04-18 15:52:59 +0100

    feat: database.py

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import DATABASE_URL
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}  # SQLite فقط
-)
+# PostgreSQL لا يحتاج check_same_thread
+connect_args = {"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
